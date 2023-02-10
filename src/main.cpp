d5ae7fe0f94cfac026d32586235504cd0f217c80
Quentin AUGEY <quentin.augey@gmail.com>
2023-02-10 18:00:33 +0100

    Beginning

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include "p6/p6.h"
 #define DOCTEST_CONFIG_IMPLEMENT
+#include <random>
 #include <vector>
+#include "Square.hpp"
 #include "doctest/doctest.h"
 
 int main(int argc, char* argv[])
@@ -19,17 +21,33 @@ int main(int argc, char* argv[])
     auto ctx = p6::Context{{.title = "progObjet"}};
     ctx.maximize_window();
 
+    p6::Angle rotation = 0.011_turn;
+    ctx.mouse_scrolled = [&](p6::MouseScroll e) {
+        rotation += e.dy * 0.025_turn;
+    };
+
+    Square s1(0.5, 0.5);
+
     // Declare your infinite update loop.
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
+        // ctx.circle(
+        //     p6::Center{ctx.mouse()},
+        //     p6::Radius{0.2f}
+        // );
+        // ctx.square(
+        //     p6::TopLeftCorner{{p6::random::number(-1, 1), p6::random::number(-1, 1)}},
+        //     p6::Radius{0.3f}
+        // );
+        s1.draw(ctx);
     };
 
     // Should be done last. It starts the infinite loop.
     ctx.start();
 
     std::vector<int> v{};
-}
+};
+
+void squares(){
+
+};
